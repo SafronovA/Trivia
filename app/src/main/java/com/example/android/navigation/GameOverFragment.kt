22ca91867e17c3b5a3e18dd98c3df57643337c3f
asafrona <aliaksei.safronau@etrade.com>
2019-11-09 19:43:48 +0300

    start

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -25,11 +25,11 @@ import androidx.fragment.app.Fragment
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_over, container, false)
-        return binding.root
-    }
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+//                              savedInstanceState: Bundle?): View? {
+//        // Inflate the layout for this fragment
+//        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
+//                inflater, R.layout.fragment_game_over, container, false)
+//        return binding.root
+//    }
 }
